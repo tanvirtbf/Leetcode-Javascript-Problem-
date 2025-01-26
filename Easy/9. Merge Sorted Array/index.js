9e68001550fe3173ac3268887c04d8cf337488bc
tanvirtbf <tanver018765@gmail.com>
2025-01-26 23:50:07 +0600

    9th problem solve

--- Easy/9. Merge Sorted Array/index.js
+++ Easy/9. Merge Sorted Array/index.js
@@ -1,17 +1,36 @@
 /**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-function plusOne(digits) {
-    for (let i = digits.length - 1; i >= 0; i--) {
-        if (digits[i] < 9) {
-            digits[i]++;
-            return digits;
-        }
-        digits[i] = 0;
-    }
+var merge = function(nums1, m, nums2, n) {
+    
+    let p = Math.floor((m + n) / 2) // 3
+    let x = 0
 
-    digits.unshift(1);
-    return digits;
-}
+    if(m===0){
+        for(let i=0; i<n; i++){
+            nums1[i]=nums2[i]
+        }
+    }else {
+        for(let i=m; i<m+n; i++){
+            nums1[i]=nums2[x]
+            x++
+        }
+    }
+    
+    // Merge 
+  for (let i = 0; i < nums1.length; i++) {
+    for (let j = 0; j < nums1.length - i - 1; j++) {
+      if (nums1[j] > nums1[j + 1]) {
+        let temp = nums1[j];
+        nums1[j] = nums1[j + 1];
+        nums1[j + 1] = temp;
+      }
+    }
+  }
+
+    return nums1
+};
