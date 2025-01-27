2e575c7a60ce2b5218f6c9d9f63cda5c24857aaf
tanvirtbf <tanver018765@gmail.com>
2025-01-27 17:53:12 +0600

    update

---
+++ Easy/17. Intersection of Two Arrays/index.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let numZero = 0
+    for(let i=0; i<nums.length; i++){
+        if(nums[i]===0){
+            numZero++
+        }
+    }
+    let index=0
+    for(let i=0; i<nums.length; i++){
+        if(nums[i]!==0){
+            nums[index] = nums[i]
+            index++
+        }
+    }
+    for(let i=nums.length-1; i>=nums.length-numZero; i--){
+        nums[i]=0
+    }
+    return nums
+};
