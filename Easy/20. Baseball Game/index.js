00c8e0eab31876b8f93503b0e7a3682d790ef6bb
tanvirtbf <tanver018765@gmail.com>
2025-01-31 21:19:51 +0600

    update

--- Easy/20. Baseball Game/index.js
+++ Easy/20. Baseball Game/index.js
@@ -1,19 +1,27 @@
 /**
- * @param {number[]} nums
- * @return {number[]}
+ * @param {string[]} operations
+ * @return {number}
  */
-var findDisappearedNumbers = function(nums) {
-    for (let i = 0; i < nums.length; i++) {
-        let index = Math.abs(nums[i]) - 1; 
-        nums[index] = -Math.abs(nums[index]); 
-    }
-
-    let result = [];
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] > 0) { 
-            result.push(i + 1); 
+var calPoints = function(operations) {
+    let arr = []
+    for(let i=0; i<operations.length; i++){
+        if(operations[i]!=="C" && operations[i]!=="D" && operations[i]!=="+"){
+            let num = Number(operations[i])
+            arr.push(num)
+        }else if(operations[i]==="C"){
+            arr.pop()
+        }else if(operations[i]==="D"){
+            let num = arr[arr.length-1] * 2
+            arr.push(num)
+        }else if(operations[i]==="+"){
+            let num = arr[arr.length-1]+arr[arr.length-2]
+            arr.push(num)
         }
     }
-    
-    return result;
+    let result = 0
+    for(let i=0; i<arr.length; i++){
+        result = result + arr[i]
+    }
+    return result
 };
+
