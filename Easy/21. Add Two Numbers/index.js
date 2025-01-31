5e8ad660a49440cd8018e7405957ea66e46832e2
tanvirtbf <tanver018765@gmail.com>
2025-01-31 21:30:29 +0600

    21th problem solve

--- Easy/21. Add Two Numbers/index.js
+++ Easy/21. Add Two Numbers/index.js
@@ -1,27 +1,37 @@
 /**
- * @param {string[]} operations
- * @return {number}
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
  */
-var calPoints = function(operations) {
-    let arr = []
-    for(let i=0; i<operations.length; i++){
-        if(operations[i]!=="C" && operations[i]!=="D" && operations[i]!=="+"){
-            let num = Number(operations[i])
-            arr.push(num)
-        }else if(operations[i]==="C"){
-            arr.pop()
-        }else if(operations[i]==="D"){
-            let num = arr[arr.length-1] * 2
-            arr.push(num)
-        }else if(operations[i]==="+"){
-            let num = arr[arr.length-1]+arr[arr.length-2]
-            arr.push(num)
-        }
-    }
-    let result = 0
-    for(let i=0; i<arr.length; i++){
-        result = result + arr[i]
-    }
-    return result
-};
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val);
+    this.next = (next === undefined ? null : next);
+}
 
+function addTwoNumbers(l1, l2) {
+    let dummyHead = new ListNode(0); 
+    let current = dummyHead; 
+    let carry = 0; 
+
+    while (l1 !== null || l2 !== null || carry !== 0) {
+        let val1 = l1 !== null ? l1.val : 0;
+        let val2 = l2 !== null ? l2.val : 0;
+
+        let sum = val1 + val2 + carry;
+        carry = Math.floor(sum / 10);
+        current.next = new ListNode(sum % 10); 
+        current = current.next; 
+
+        if (l1 !== null) l1 = l1.next;
+        if (l2 !== null) l2 = l2.next;
+    }
+
+    return dummyHead.next; 
+}
